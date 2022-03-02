607a7a60ad2c7ccdc77383ce2b42f6633db17fd9
tlancaster6 <tlancaster6@gatech.edu>
2022-03-02 15:09:25 -0500

    initial commit

---
+++ install_requirements.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install libedgetpu1-std
+sudo apt-get install python3-pycoral
+if grep -s -q "Mendel" /etc/os-release; then
+  echo "No DevBoard specific dependencies"
+else
+  # Install gstreamer
+  sudo apt-get install -y gstreamer1.0-plugins-bad gstreamer1.0-plugins-good python3-gst-1.0 python3-gi gir1.2-gtk-3.0
+
+  if grep -s -q "Raspberry Pi" /sys/firmware/devicetree/base/model; then
+    echo "Installing Raspberry Pi specific dependencies"
+    sudo apt-get install python3-rpi.gpio
+    # Add v4l2 video module to kernel
+    if ! grep -q "bcm2835-v4l2" /etc/modules; then
+      echo bcm2835-v4l2 | sudo tee -a /etc/modules
+    fi
+    sudo modprobe bcm2835-v4l2
+  fi
+fi
