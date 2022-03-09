c516d400b89fd7b906f030021eaf3abf9d2337cf
tlancaster6 <tlancaster6@gatech.edu>
2022-03-09 16:20:48 -0500

    on-device debugging

--- internet_of_fish/main.py
+++ internet_of_fish/main.py
@@ -1,6 +1,9 @@
 from modules import definitions
 from modules.manager import Manager
 import argparse
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--pid')
