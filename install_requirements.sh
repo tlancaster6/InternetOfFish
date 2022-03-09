fc3915d8304d1a98148e786edb6c9ca07cede621
tlancaster6 <tlancaster6@gatech.edu>
2022-03-09 12:29:35 -0500

    adding git pull to setup script

--- install_requirements.sh
+++ install_requirements.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+git pull
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 sudo apt-get update
