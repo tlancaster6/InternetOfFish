adf7de46fef2f21d787666de67cfb2243d9ba4a9
tlancaster6 <tlancaster6@gatech.edu>
2022-03-15 13:33:53 -0400

    adjusting import scope

--- internet_of_fish/main.py
+++ internet_of_fish/main.py
@@ -2,6 +2,7 @@ import argparse, time, logging
 import os.path
 import sys
 sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+print(sys.path)
 from internet_of_fish.modules.collector import CollectorWorker
 from internet_of_fish.modules.detector import DetectorWorker
 from internet_of_fish.modules.contexts import MainContext
