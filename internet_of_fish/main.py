e070b59208958fe3eabfc9d022b9a187cc2bb6de
tlancaster6 <tlancaster6@gatech.edu>
2022-03-15 13:22:22 -0400

    commenting out some unused code

--- internet_of_fish/main.py
+++ internet_of_fish/main.py
@@ -23,34 +23,34 @@ def main(args):
                                  iof_signals.default_signal_handler)
 
         img_q = main_ctx.MPQueue()
-        send_q = main_ctx.MPQueue()
-        reply_q = main_ctx.MPQueue()
+        # send_q = main_ctx.MPQueue()
+        # reply_q = main_ctx.MPQueue()
 
         main_ctx.Proc('COLLECT', CollectorWorker, img_q, proj_id)
         main_ctx.Proc('DETECT', DetectorWorker, img_q, proj_id, model_id)
 
         while not main_ctx.shutdown_event.is_set():
             if die_time and time.time() > die_time:
-                raise RuntimeError("Application has run too long.")
+                main_ctx.shutdown_event.set()
             event = main_ctx.event_queue.safe_get()
             if not event:
                 continue
-            elif event.msg_type == "STATUS":
-                send_q.put(event)
-            elif event.msg_type == "OBSERVATION":
-                send_q.put(event)
-            elif event.msg_type == "ERROR":
-                send_q.put(event)
-            elif event.msg_type == "REQUEST":
-                request_handler(event, reply_q, main_ctx)
-            elif event.msg_type == "FATAL":
-                main_ctx.log(logging.INFO, f"Fatal Event received: {event.msg}")
-                break
-            elif event.msg_type == "END":
-                main_ctx.log(logging.INFO, f"Shutdown Event received: {event.msg}")
-                break
-            else:
-                main_ctx.log(logging.ERROR, f"Unknown Event: {event}")
+            # elif event.msg_type == "STATUS":
+            #     send_q.put(event)
+            # elif event.msg_type == "OBSERVATION":
+            #     send_q.put(event)
+            # elif event.msg_type == "ERROR":
+            #     send_q.put(event)
+            # elif event.msg_type == "REQUEST":
+            #     request_handler(event, reply_q, main_ctx)
+            # elif event.msg_type == "FATAL":
+            #     main_ctx.log(logging.INFO, f"Fatal Event received: {event.msg}")
+            #     break
+            # elif event.msg_type == "END":
+            #     main_ctx.log(logging.INFO, f"Shutdown Event received: {event.msg}")
+            #     break
+            # else:
+            #     main_ctx.log(logging.ERROR, f"Unknown Event: {event}")
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
