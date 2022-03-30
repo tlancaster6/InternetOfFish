a859052668de92a992ad93231916881bf9c688b1
tlancaster6 <tuckerlancaster@gmail.com>
2022-03-30 11:23:54 -0400

    writing tests

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ from context import utils
                          [(10, 10, 100, 10),
                           (90, 20, 100, 10),
                           (100, 10, 90, 0),
-                          100, 10, 100, 0])
+                          (100, 10, 100, 0)])
 
 def test_max_sleep(mocker, curr_time, max_sleep, end_time, expected_time):
     mocker.patch('utils.time.time', return_value=curr_time)
