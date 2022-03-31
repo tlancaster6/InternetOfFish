157db3e1535f9b3c803cef396299934a93ef2d3b
tlancaster6 <tuckerlancaster@gmail.com>
2022-03-31 11:18:39 -0400

    writing tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -17,18 +17,28 @@ def mock_metadata():
 def testing_context(mock_metadata):
     yield mptools.MainContext(mock_metadata)
 
-
-def test_daytime_startup(mocker, testing_context):
+@pytest.mark.parametrize('mode', ['active', 'passive'])
+def test_runner_startup(mocker, testing_context, mode):
     mocker.patch('context.runner')
     with testing_context as main_ctx:
         mptools.init_signals(main_ctx.shutdown_event, mptools.default_signal_handler, mptools.default_signal_handler)
-        mocker.patch('context.runner.RunnerWorker.return_value.expected_mode', return_value='active')
+        mocker.patch('context.runner.RunnerWorker.return_value.expected_mode', return_value=mode)
         runner_proc = main_ctx.Proc('RUN', runner.RunnerWorker, main_ctx, persistent=True)
-        tries_left = 3
-        while tries_left:
-            time.sleep(4-tries_left)
-            if runner_proc.startup_event.is_set():
-                break
+        runner_proc.startup_event.wait(10)
         assert runner_proc.startup_event.is_set()
+
+@pytest.mark.parametrize('mode', ['active', 'passive'])
+def test_runner_shutdown(mocker, testing_context, mode):
+    mocker.patch('context.runner')
+    with testing_context as main_ctx:
+        mptools.init_signals(main_ctx.shutdown_event, mptools.default_signal_handler, mptools.default_signal_handler)
+        mocker.patch('context.runner.RunnerWorker.return_value.expected_mode', return_value=mode)
+        runner_proc = main_ctx.Proc('RUN', runner.RunnerWorker, main_ctx, persistent=True)
+        runner_proc.startup_event.wait(10)
+        runner_proc.shutdown_event.wait(10)
+        assert runner_proc.shutdown_event.is_set()
+
+
+
 
 
