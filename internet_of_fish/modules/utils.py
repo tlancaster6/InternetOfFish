ead6a7916e1c97e427347b138ba9c137022167a1
tlancaster6 <tuckerlancaster@gmail.com>
2022-03-31 15:44:09 -0400

    cleaning up log formatting

--- internet_of_fish/modules/utils.py
+++ internet_of_fish/modules/utils.py
@@ -4,7 +4,7 @@ from internet_of_fish.modules import definitions
 import os, socket, cv2
 
 LOG_DIR, LOG_LEVEL = definitions.LOG_DIR, definitions.LOG_LEVEL
-LOG_FMT = logging.Formatter(fmt='%(asctime)s %(name)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+LOG_FMT = logging.Formatter(fmt='%(asctime)s %(name)-8s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 os.makedirs(LOG_DIR, exist_ok=True)
 logging.getLogger('PIL').setLevel(logging.WARNING)
 
@@ -45,7 +45,7 @@ def make_logger(name):
     if not os.path.exists(definitions.LOG_DIR):
         os.makedirs(definitions.LOG_DIR, exist_ok=True)
     logging.basicConfig(
-        format='%(asctime)s %(name)s %(levelname)-8s %(message)s',
+        format='%(asctime)s %(name)-8s %(levelname)-8s %(message)s',
         level=LOG_LEVEL,
         datefmt='%Y-%m-%d %H:%M:%S')
     logger = logging.getLogger(name)
