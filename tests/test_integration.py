e267230100d7a3777b5b965fc68f33a037748bcd
tlancaster6 <tuckerlancaster@gmail.com>
2022-03-31 12:36:16 -0400

    writing tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -19,7 +19,6 @@ def testing_context(mock_metadata):
 
 @pytest.mark.parametrize('mode', ['active', 'passive'])
 def test_runner_startup(mocker, testing_context, mode):
-    mocker.patch('context.runner')
     with testing_context as main_ctx:
         mptools.init_signals(main_ctx.shutdown_event, mptools.default_signal_handler, mptools.default_signal_handler)
         mocker.patch('context.runner.RunnerWorker.return_value.expected_mode', return_value=mode)
@@ -33,7 +32,6 @@ def test_runner_startup(mocker, testing_context, mode):
 
 @pytest.mark.parametrize('mode', ['active', 'passive'])
 def test_runner_hard_shutdown(mocker, testing_context, mode):
-    mocker.patch('context.runner')
     with testing_context as main_ctx:
         mptools.init_signals(main_ctx.shutdown_event, mptools.default_signal_handler, mptools.default_signal_handler)
         mocker.patch('context.runner.RunnerWorker.return_value.expected_mode', return_value=mode)
@@ -50,7 +48,6 @@ def test_runner_hard_shutdown(mocker, testing_context, mode):
 
 @pytest.mark.parametrize('mode', ['active', 'passive'])
 def test_runner_soft_shutdown(mocker, testing_context, mode):
-    mocker.patch('context.runner')
     with testing_context as main_ctx:
         mptools.init_signals(main_ctx.shutdown_event, mptools.default_signal_handler, mptools.default_signal_handler)
         mocker.patch('context.runner.RunnerWorker.return_value.expected_mode', return_value=mode)
---
+++ tests/test_mocks.py
@@ -0,0 +1,23 @@
+from context import runner, metadata, mptools
+import pytest
+
+@pytest.fixture
+def mock_metadata():
+    mm = metadata.MetaDataDict()
+    mm.quick_update({'owner': 'foo',
+                     'species': 'bar',
+                     'fish_type': 'other',
+                     'model_id': 'efficientdet',
+                     })
+    yield mm.simplify()
+
+@pytest.fixture
+def testing_context(mock_metadata):
+    yield mptools.MainContext(mock_metadata)
+
+def test_runner_subproc_mocks(mocker, testing_context):
+    with testing_context as main_ctx:
+        mptools.init_signals(main_ctx.shutdown_event, mptools.default_signal_handler, mptools.default_signal_handler)
+        mocker.patch('context.runner.collector.CollectorWorker.return_value')
+        mocker.patch('context.runner.detector.DetectorWorker.return_value')
+        print(type(runner.collector.CollectorWorker))
