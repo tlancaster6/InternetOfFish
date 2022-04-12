24375c5aaf8ec9feb68f1e466a049df2e36b35c4
tlancaster6 <tuckerlancaster@gmail.com>
2022-04-12 13:22:06 -0400

    new ui

--- internet_of_fish/ui.py
+++ internet_of_fish/ui.py
@@ -2,7 +2,7 @@ import os, sys
 if os.path.abspath(os.path.dirname(os.path.dirname(__file__))) not in sys.path:
     sys.path.insert(0, os.path.abspath(os.path.dirname(os.path.dirname(__file__))))
 from internet_of_fish.modules import ui_helpers, utils, metadata, mptools, runner
-import colorama
+import colorama, time
 colorama.init(autoreset=True)
 
 
@@ -123,6 +123,7 @@ class UI:
         if self.main_ctx:
             self.main_ctx.__exit__()
         utils.cprint('goodbye')
+        time.sleep(1)
         sys.exit()
 
 if __name__ == '__main__':
