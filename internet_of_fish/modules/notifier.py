2f114e7f20e38ea429316548eb394f69349e8b06
tlancaster <tlancaster6@gatech.edu>
2022-04-13 10:54:46 -0400

    working on SourceCollectorWorker

--- internet_of_fish/modules/notifier.py
+++ internet_of_fish/modules/notifier.py
@@ -51,11 +51,9 @@ class NotifierWorker(mptools.QueueProcWorker, metaclass=utils.AutologMetaclass):
             except Exception as e:
                 self.logger.debug(f'failed to send message. {tries_left} tries remaining')
                 print(e)
-        # else:
-        #     self.logger.warning(f'failed to send message {self.defs.MAX_TRIES} times')
-        #     self.logger.debug(f'response status code: {response.status_code}')
-        #     self.logger.debug(f'response body: {response.body}')
-        #     self.logger.debug(f'response headers: {response.headers}')
+            self.logger.debug(f'response status code: {response.status_code}')
+            self.logger.debug(f'response body: {response.body}')
+            self.logger.debug(f'response headers: {response.headers}')
 
     def check_notification_conditions(self, notification: Notification):
         if not self.last_notification:
