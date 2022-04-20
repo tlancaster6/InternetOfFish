857cfcb27bfd894de8140cb6e6fe44b0205d80c9
tlancaster6 <tlancaster6@gatech.edu>
2022-04-20 10:14:47 -0400

    cleaning up config scripts

--- bin/configure_controller.sh
+++ bin/configure_controller.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 pip3 install fabric
 pip3 install capturer
--- bin/configure_worker.sh
+++ bin/configure_worker.sh
@@ -4,6 +4,21 @@ echo 'updating repository'
 cd ~/InternetOfFish
 git reset --hard HEAD
 git pull
+# install requirements
+echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install libedgetpu1-std
+sudo apt-get -y install python3-pycoral
+curl https://rclone.org/install.sh | sudo bash
+sudo apt-get -y install screen
+sudo pip3 install rclone
+sudo pip3 install picamera
+sudo pip3 install sendgrid
+sudo pip3 install opencv-python-headless
+sudo pip3 install ffmpeg
+sudo pip3 install -U numpy
+sudo apt-get -y install libatlas-base-dev
 # generate a crontab entry to restart data collection every time the pi reboots
 echo 'setting up cron job'
 (crontab -l ; echo "@reboot ~/InternetOfFish/bin/unit_scripts/auto_start.sh" ) | sort - | uniq - | crontab -
